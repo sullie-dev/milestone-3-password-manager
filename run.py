2bce35580b2575467d23a34503d76700d0eff8c4
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-11-17 11:47:46 +0000

    create login function to check if the password user provides

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def login():
+    """Check to make sure the master password is correct"""
+    password = input("Enter the master password: ")
+    master_password = "pw123"
+    if password == master_password:
+        print("Kittridge, You've Never Seen Me Very Upset!")
+        return True
+    else:
+        print("Oh no, the password... it's wrong")
+        return False
+
+
+login()
