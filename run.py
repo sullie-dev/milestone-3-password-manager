33a9e1131d9156c8456a15c02c9b800f6716ab74
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-11-19 15:25:20 +0000

    Add create password function to add a new password to the database. Refactor random password function to create a new password record if needed
    Signed-off-by: Eoghan O'Sullivan <eoghan@sullie.dev>

--- run.py
+++ run.py
@@ -26,6 +26,29 @@ def connect_db():
         print(e)
 
 
+def create_password(connection, password=None):
+    """Allow user to create a new database entry"""
+    username = input("Please enter the username ")
+    if password is None:
+        password = input("Please enter your password ")
+    url = input('Please enter the url of the website')
+    password_name = input("What would you like to name this password?")
+
+    connection = connection
+    cursor = connection.cursor()
+
+    insert_query = """ INSERT INTO passwords (username, password, url, password_name) VALUES (%s,%s,%s,%s)"""
+    record_to_insert = (username, password, url, password_name)
+    cursor.execute(insert_query, record_to_insert)
+
+    print("Adding your password.")
+    print("Adding your password..")
+    print("Adding your password...")
+
+    connection.commit()
+    print("Password added to the database")
+
+
 def generate_password(length):
     """Generates a random password for the user"""
     upper_string = string.ascii_uppercase
@@ -36,17 +59,25 @@ def generate_password(length):
     alphabet = upper_string + lower_string + numerical + symbol
     random_character = random.sample(alphabet, length)
     random_password = "".join(random_character)
+
+    add_new_password = input("Do you want to enter this as a new password? ")
+
+    if add_new_password == "yes":
+        create_password(connect_db(), random_password)
+    else:
+        return random_password
+
     return random_password
 
 
 def menu():
-    """Allows the user to be able to picj which option they want to select"""
+    """Allows the user to be able to pick which option they want to select"""
     print("What would you like to do?")
     menu_option = int(input("1. Add new password\n2. Generate a new password\n"))
 
     try:
         if menu_option == 1:
-            print("menu option 1")
+            create_password(connect_db())
         elif menu_option == 2:
             length = int(input("How long do you want the password to be? "))
             print(generate_password(length))
@@ -63,7 +94,7 @@ def main():
 
     while True:
         is_logged_in = login()
-        if is_logged_in:
+        while is_logged_in:
             print("But... we're in")
             menu()
 
