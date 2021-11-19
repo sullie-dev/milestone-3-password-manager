5386cd6a978c386435d4ec89bf73b2cc5bf743bc
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-11-19 11:57:26 +0000

    Remove master password string, create function for connecting to database

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
+import os
+import psycopg2
+
+
 def login():
     """Check to make sure the master password is correct"""
     password = input("Enter the master password: ")
-    master_password = "pw123"
+    master_password = os.getenv("MASTER_PASSWORD")
     if password == master_password:
         print("Kittridge, You've Never Seen Me Very Upset!")
         return True
@@ -10,13 +14,24 @@ def login():
         return False
 
 
+def connect_db():
+    try:
+        database_url = os.getenv("DATABASE_URL")
+        db_connection = psycopg2.connect(database_url, sslmode="require")
+        return db_connection
+    except Exception as e:
+        print(e)
+
+
 def main():
     """Main function"""
+
+    print(connect_db())
+
     while True:
         is_logged_in = login()
-        if is_logged_in == True:
+        if is_logged_in:
             print("But... we're in")
-            break
 
 
 main()
